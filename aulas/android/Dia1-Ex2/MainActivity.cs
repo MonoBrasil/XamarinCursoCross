486f17a8fab2120a179adfffc1089bca8f217070
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2013-12-28 18:52:51 -0300

    Dia 1 - Exemplo 2 Relative Layout - http://docs.xamarin.com/guides/android/user_interface/table_layout/

---
+++ aulas/android/Dia1-Ex2/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Dia1Ex2
+{
+	[Activity (Label = "Dia1-Ex2", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Coloca o layout "main" dos recursos em nossa view
+			SetContentView (Resource.Layout.Main);
+		}
+	}
+}
+
+
