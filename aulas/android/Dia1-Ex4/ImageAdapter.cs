2806093951317d069981904e328e6a500689aaa5
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@gmail.com>
2013-12-28 19:18:14 -0300

    Dia 1 - Exemplo 4 Grid View - http://docs.xamarin.com/guides/android/user_interface/grid_view

---
+++ aulas/android/Dia1-Ex4/ImageAdapter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Android.Widget;
+using Android.Content;
+using Android.Views;
+
+namespace Dia1Ex4
+{
+	public class ImageAdapter : BaseAdapter
+	{
+		Context context;
+
+		public ImageAdapter (Context c)
+		{
+			context = c;
+		}
+
+		public override int Count {
+			get { return thumbIds.Length; }
+		}
+
+		public override Java.Lang.Object GetItem (int position)
+		{
+			return null;
+		}
+
+		public override long GetItemId (int position)
+		{
+			return 0;
+		}
+
+		// create a new ImageView for each item referenced by the Adapter
+		public override View GetView (int position, View convertView, ViewGroup parent)
+		{
+			ImageView imageView;
+
+			if (convertView == null) {  // if it's not recycled, initialize some attributes
+				imageView = new ImageView (context);
+				imageView.LayoutParameters = new GridView.LayoutParams (85, 85);
+				imageView.SetScaleType (ImageView.ScaleType.CenterCrop);
+				imageView.SetPadding (8, 8, 8, 8);
+			} else {
+				imageView = (ImageView)convertView;
+			}
+
+			imageView.SetImageResource (thumbIds[position]);
+			return imageView;
+		}
+
+		// references to our images
+		int[] thumbIds = {
+			Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+			Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+			Resource.Drawable.sample_6, Resource.Drawable.sample_7,
+			Resource.Drawable.sample_0, Resource.Drawable.sample_1,
+			Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+			Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+			Resource.Drawable.sample_6, Resource.Drawable.sample_7,
+			Resource.Drawable.sample_0, Resource.Drawable.sample_1,
+			Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+			Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+			Resource.Drawable.sample_6, Resource.Drawable.sample_7
+		};
+	}
+}
+
---
+++ aulas/android/Dia1-Ex4/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Dia1Ex4
+{
+	[Activity (Label = "Dia1-Ex4", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Coloca o layout "main" dos recursos em nossa view
+			SetContentView (Resource.Layout.Main);
+
+			var gridview = FindViewById<GridView> (Resource.Id.gridview);
+			gridview.Adapter = new ImageAdapter (this);
+
+			gridview.ItemClick += delegate (object sender, AdapterView.ItemClickEventArgs args) {
+				Toast.MakeText (this, args.Position.ToString (), ToastLength.Short).Show ();
+			};
+		}
+	}
+}
+
+
